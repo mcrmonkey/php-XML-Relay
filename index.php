73ced3f09e423b2f63843645fca5c9b9ff51cfb4
Ant Kenworthy <git@manchestermonkey.co.uk>
2016-06-01 16:26:33 +0100

    Initial add
    + index.php
    + readme.md

---
+++ index.php
@@ -0,0 +1,25 @@
+<?
+$url = 'http://destination.host/path';
+
+
+$xml = file_get_contents('php://input');
+
+$myvars = $xml;
+
+$ch = curl_init();
+curl_setopt($ch, CURLOPT_URL, $url);
+curl_setopt($ch, CURLOPT_HEADER, 1);
+curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
+curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'POST');
+curl_setopt($ch, CURLOPT_POST, 1);
+curl_setopt($ch, CURLOPT_POSTFIELDS, $xml);
+curl_setopt($ch, CURLOPT_HTTPHEADER, array(
+                                       'Content-type: application/xml', 
+                                       'Content-length: ' . strlen($xml)
+                                     ));
+$output = curl_exec($ch);
+curl_close($ch);
+
+?>
---
+++ readme.md
@@ -0,0 +1,14 @@
+PHP-XML-Relay
+=============
+
+Created to overcome a firewalling issue where the final target isn't directly accessable from the sending host but it can be accessed via an intermediary.
+
+Script was designed to operate with XML output from another system.
+
+
+Requirements
+------------
+
+You'll need a machine running PHP and capable of of taking a POST containing XML data
+
+The php5-curl module will also be required to perform the relay.
